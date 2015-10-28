d6cabf9d6121befbdab96a15dfed1f8f9940283b
Raghuram Kalyanam <kalyanam@rhrk.uni-kl.de>
2015-10-28 14:30:09 +0100

    Add description in run_batchcreateschedules.sh file

--- run_batchcreateschedules.sh
+++ run_batchcreateschedules.sh
@@ -4,6 +4,7 @@ if [ $# -eq 3 ]; then
 	
 	cd batch
         ./climate.sh $1 $2 $3 &
+# Here the cimate means the place name and the parameters 1 represents the climate, 2 represents no. of the layers and 3 represents the positon of the shading layer. Please refer to the manual.
 else
     echo "Your command line doesn't have enough arguments"
 fi
